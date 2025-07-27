cc1f078e0383fd749f42e164928532ea543e09f4
412449-PICCO <412449@tecnicatura.frc.utn.edu.ar>
2025-07-26 22:47:04 -0300

    .

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,8 +1,47 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {EmailJSResponseStatus} from '@emailjs/browser';
+import * as emailjs from '@emailjs/browser';
+
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
+  imports: [
+    FormsModule
+  ],
   styleUrls: ['./contacto.component.css']
 })
-export class ContactoComponent {}
+export class ContactoComponent {
+
+
+  formData = {
+    nombre: '',
+    empresa: '',
+    email: '',
+    telefono: '',
+    servicio: '',
+    presupuesto: '',
+    mensaje: ''
+  };
+
+  sendEmail(event: Event) {
+    event.preventDefault();
+
+    emailjs.send(
+      'service_erf8jwh',   // Reemplaza con tu SERVICE ID de EmailJS
+      'template_t9dmqcn',  // Reemplaza con tu TEMPLATE ID de EmailJS
+      this.formData,
+      'lb9k0kOGcJPOFtSPd'    // Reemplaza con tu PUBLIC KEY de EmailJS
+    ).then((result: EmailJSResponseStatus) => {
+      console.log(result.text);
+      alert('Mensaje enviado correctamente.');
+    }, (error) => {
+      console.error(error.text);
+      alert('Hubo un error al enviar el mensaje. Inténtalo de nuevo.');
+    });
+  }
+
+
+
+}
